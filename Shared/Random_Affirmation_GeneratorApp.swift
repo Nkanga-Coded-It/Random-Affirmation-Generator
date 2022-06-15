e7abaca4c71cd3ef94a3c8caee0f15d4c66f3791
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-06-15 15:46:59 +0100

    Initial Commit

---
+++ Shared/Random_Affirmation_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Affirmation_GeneratorApp.swift
+//  Shared
+//
+//  Created by Richard Nkanga on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Random_Affirmation_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
